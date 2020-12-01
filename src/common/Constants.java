fe49fc48af9673b7ee1edf83308e360abd5961dc
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2020-12-01 23:53:13 +0200

    Some constants added

---
+++ src/common/Constants.java
@@ -0,0 +1,9 @@
+package common;
+
+public class Constants
+{
+	static final int MIN_ROW_VALUE = 0;
+	static final int MAX_ROW_VALUE = 0;
+	static final int MIN_COLUMN_VALUE = 0;
+	static final int MAX_COLUMN_VALUE = 0;
+}
