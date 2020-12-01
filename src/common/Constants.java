dcd9378fdb57fedcf569a2432ee7d43435202d32
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2020-12-01 23:55:30 +0200

    Made all fields public

--- src/common/Constants.java
+++ src/common/Constants.java
@@ -2,8 +2,8 @@ package common;
 
 public class Constants
 {
-	static final int MIN_ROW_VALUE = 0;
-	static final int MAX_ROW_VALUE = 0;
-	static final int MIN_COLUMN_VALUE = 0;
-	static final int MAX_COLUMN_VALUE = 0;
+	public static final int MIN_ROW_VALUE = 0;
+	public static final int MAX_ROW_VALUE = 0;
+	public static final int MIN_COLUMN_VALUE = 0;
+	public static final int MAX_COLUMN_VALUE = 0;
 }
