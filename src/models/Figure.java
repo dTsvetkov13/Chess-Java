35d7707e05708ce9c46751babf8e91199f7a97ec
admin-45053 <admin-45053@admin-45053-PC>
2020-12-04 19:39:07 +0200

    Figure in it

---
+++ src/models/Figure.java
@@ -0,0 +1,80 @@
+package models;
+
+import enums.FigureType;
+import enums.Team;
+
+public abstract class Figure 
+{
+	private Cordinates coordinates;
+	private Team team;
+	private boolean isMoved;
+	private FigureType type;
+	private Cordinates[] reachableCells;
+	private int reachableCellsCount;
+	
+	public Figure(Cordinates coordinates, Team team, FigureType type)
+	{
+		if(coordinates != null)
+		{
+			this.coordinates = coordinates;
+			this.team = team;
+			this.isMoved = false;
+			this.type = type;
+			this.reachableCells = new Cordinates[32]; // TO DO: make constant for it
+		}
+	}
+	
+	public Cordinates getCoordinates()
+	{
+		return coordinates;
+	}
+	
+	public Team getTeam()
+	{
+		return team;
+	}
+	
+	public boolean isMoved()
+	{
+		return isMoved;
+	}
+	
+	public FigureType getType()
+	{
+		return type;
+	}
+	
+	public Cordinates[] getReachableCells()
+	{
+		return reachableCells;
+	}
+	
+	public int getReachableCellsCount()
+	{
+		return reachableCellsCount;
+	}
+	
+	public abstract int CalculateReachableCells(Cordinates row, Cordinates column);
+	
+	public boolean isOneOfReachableCells(Cordinates coordinates)
+	{
+		if(reachableCellsCount > 0)
+		{
+			for(int i = 0; i < reachableCellsCount; i++)
+			{
+				if(reachableCells[i].getRow() == coordinates.getRow() && 
+						reachableCells[i].getColumn() == coordinates.getColumn())
+				{
+					return true;
+				}
+			
+			}
+		}
+		return false;
+	}
+	
+	public boolean canYouReachEnemyKing()
+	{
+		return false;  
+	}
+}
