c92b11d1b567bcf55cb8d550dd5e06f613019506
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2020-12-05 12:54:08 +0200

    CellView init

---
+++ src/views/CellView.java
@@ -0,0 +1,24 @@
+package views;
+
+import java.awt.Graphics;
+
+import javax.swing.JComponent;
+
+public class CellView extends JComponent
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public CellView()
+	{
+		
+	}
+	
+	@Override
+	public void paintComponent(Graphics g)
+	{
+		
+	}
+}
