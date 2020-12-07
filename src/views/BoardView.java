8a23a5f8f44895752a49735c68167812c6a23080
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2020-12-07 22:34:19 +0200

    Two drawing methods implemented

--- src/views/BoardView.java
+++ src/views/BoardView.java
@@ -1,28 +1,46 @@
 package views;
 
 import java.awt.Graphics;
-import javax.swing.JPanel;
-import models.Coordinates;
 
+import javax.swing.JPanel;
+
+import models.Board;
+import models.Coordinates;
+import models.Figure;
 
 public class BoardView extends JPanel
-{
-	//This is required by JComponent
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
+{	
 	private Coordinates lastClickedCell;
+	private Board board;
 	
 	public BoardView()
 	{
-		
+		board = Board.getInstance();
 	}
 	
 	@Override
 	public void paintComponent(Graphics g)
 	{
+		Figure[][] figures = board.getAllFigures();
+		CellView tempCell;
 		
+		for(int i = 0; i < figures.length; i++)
+		{
+			for(int j = 0; j < figures[i].length; j++)
+			{
+				tempCell = new CellView(figures[i][j]);
+				tempCell.setBounds(null); //TODO: set bounds
+				this.add(tempCell);
+			}
+		}
+	}
+	
+	@Override
+	public void repaint()
+	{
+		if(this.getGraphics() != null)
+		{
+			this.printComponent(this.getGraphics());
+		}
 	}
 }
