b7f0c7ecad1ab3d72e741c045ebad9dea4d08712
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2020-12-19 17:48:31 +0200

    onCellClicked added

--- src/models/GameEventListener.java
+++ src/models/GameEventListener.java
@@ -2,6 +2,7 @@ package models;
 
 import models.figures.Figure;
 import views.BoardView;
+import views.CellView;
 
 public interface GameEventListener
 {
@@ -9,7 +10,7 @@ public interface GameEventListener
 	void onFigureTaken();
 	void onGameOver(Game game);
 	void onGameStart(Game game);
-	
+	void onCellClicked(CellView cellView);
 	void onFigureClicked(Figure figure);
 	void onDestinationClicked(Coordinates from, Coordinates to);
 }
