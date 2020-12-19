58155dce7179440d7a75d595a01cd4c5ddd1229e
kids <kids@petia>
2020-12-19 13:14:14 +0200

    Added setters and getters in MovementInDirection

--- src/models/MovementInDirection.java
+++ src/models/MovementInDirection.java
@@ -5,11 +5,31 @@ import models.figures.Figure;
 
 public class MovementInDirection {
 	private Directions direction;
-	private Figure[] move;
+	private Coordinates[] move;
 	
-	public MovementInDirection(Directions directionValue, Figure[] moveValue)
+	public MovementInDirection(Directions directionValue, Coordinates[] moveValue)
 	{
 		this.direction = directionValue;
 		this.move = moveValue;
+	}
+	
+	public void setDirection(Directions directionValue)
+	{
+		this.direction = directionValue;
+	}
+	
+	public void setMove(Coordinates[] moveValue)
+	{
+		this.move = moveValue;
+	}
+	
+	public Directions getDirection()
+	{
+		return this.direction;
+	}
+	
+	public Coordinates[] getMove()
+	{
+		return this.move;
 	}
 }
