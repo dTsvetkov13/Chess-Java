bd9c7c7065841888c354747490218404b55c05d5
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2020-12-19 21:22:19 +0200

    MouseListener's part removed

--- src/views/BoardView.java
+++ src/views/BoardView.java
@@ -13,9 +13,7 @@ import models.Coordinates;
 import models.figures.Figure;
 
 public class BoardView extends JPanel
-					   implements MouseListener
-{	
-	private Coordinates lastClickedCell;
+{
 	private Board board;
 	
 	public BoardView()
@@ -69,70 +67,5 @@ public class BoardView extends JPanel
 		{
 			this.printComponent(this.getGraphics());
 		}
-	}
-
-	@Override
-	public void mouseClicked(MouseEvent mouseEvent)
-	{
-		int x = mouseEvent.getX();
-		int y = mouseEvent.getY();
-		
-		Coordinates figureCoor = this.calculateExactCoordinates(x, y);
-		
-		Figure clickedFigure = Board.getInstance().getFigure(figureCoor);
-		
-		if(clickedFigure != null) 
-		{
-			//if(clickedFigure.getTeam().equals(currentPlayer.getTeam()))
-			{
-				lastClickedCell = clickedFigure.getCoordinates();
-				//call listener onFigureClicked
-			}
-			//else if(Board.getInstance().getFigure(lastClickedCell).getTeam() == currentPlayer.getTeam())
-			{
-				 //call listener onDestinationClicked
-			}
-		}
-		else
-		{
-			//Click again
-		}
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent mouseEvent)
-	{		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent mouseEvent)
-	{
-	}
-
-	@Override
-	public void mousePressed(MouseEvent mouseEvent)
-	{	
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent mouseEvent)
-	{	
-	}
-	
-	private Coordinates calculateExactCoordinates(int x, int y)
-	{
-		int cellWidth = this.getWidth() / (Constants.MAX_COLUMN_VALUE + 1);
-		int cellHeight = this.getHeight() / (Constants.MAX_ROW_VALUE + 1);
-		
-		int row = y / cellHeight; //TODO: see if it will break when y is max
-		int column = x / cellWidth; //TODO: see if it will break when x is max
-		
-		if(true) //playerOnTurn.getTeam().equals(Black)
-		{
-			row = Constants.MAX_ROW_VALUE - row;
-			column = Constants.MAX_COLUMN_VALUE - column;
-		}
-		
-		return new Coordinates(row, column);
 	}
 }
