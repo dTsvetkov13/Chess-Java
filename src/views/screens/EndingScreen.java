f7f9be5a12c300492abbeede0a1a30698d297adc
Gerganadd <gerganadoncheva2003@gmail.com>
2020-12-20 21:23:42 +0200

    added GridBagLayout

--- src/views/screens/EndingScreen.java
+++ src/views/screens/EndingScreen.java
@@ -1,6 +1,9 @@
 package views.screens;
 
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,90 +12,68 @@ import javax.swing.JLabel;
 
 public class EndingScreen extends Screen
 {
-	public final static int FIRST_ROW_X = 85;
-	public final static int FIRST_ROW_Y = 30;
+	public final static int SPACE = 15;
+	
+	public final static int LBL_WINER_ROW = 0;
+	public final static int BTN_NEW_GAME_ROW = 1;
+	public final static int BTN_EXIT_ROW = 2;
 	
 	public final static int FONT_SIZE = 30;
-	public final static int LABLES_HIGHT = 30;
 	
-	public final static int SPACE = 20;
+	private GridBagConstraints gbc;
 	
-	public final static int BTN_WIDTH = 300;
-	public final static int BTN_HEIGHT = 65;
-	
-	public final static int BTN_X = (SCREEN_WIDTH - BTN_WIDTH) / 2 - 8;
-	public final static int FIRST_BTN_Y = 200;
-	
-	
-	public EndingScreen(String name) {
+	public EndingScreen(String name) 
+	{
 		super(name);
+		
+		this.setLayout(new GridBagLayout());
+		this.gbc = new GridBagConstraints();
+		
 		drawEndingScreen();
 	}
 
 	public void drawEndingScreen()
 	{
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.insets = new Insets(SPACE, SPACE, SPACE, SPACE); //external padding
+		
 		drawLables();
 		drawBtnNewGame();
 		drawBtnExit();
 	}
 	
-	public int centerHorizontallyX(String text)//add better name
-	{
-		int size = text.length();
-
-		if(size % 2 == 1) size++;
-		
-		return ((SCREEN_WIDTH - size) / 2);
-	}
-	
 	private void drawLables()
 	{
-		int x = FIRST_ROW_X; 
-		int y = FIRST_ROW_Y;
-		
-		JLabel congratilations = new JLabel("Congratulations\n");
-		
-		congratilations.setFont(new Font("Arial", Font.PLAIN, FONT_SIZE));
-		congratilations.setBounds(x, y, SCREEN_WIDTH - x, LABLES_HIGHT);
-		
-		this.add(congratilations);
-		
-		String player = "Johnny63_13";//change with name of Player class
+		gbc.gridy = LBL_WINER_ROW;
 		
 		JLabel winer = new JLabel();
+		String player = "Johnny63_13";//change with name of Player class
 		
-		winer.setText(player + " win");
+		winer.setText(player + " wins");
 		winer.setFont(new Font("Arial", Font.PLAIN, FONT_SIZE)); 
 		
-		x = centerHorizontallyX(winer.getText());
-		y += LABLES_HIGHT + SPACE; 
-		
-		winer.setBounds(x, y, SCREEN_WIDTH - x, LABLES_HIGHT);
-		
-		this.add(winer);
+		this.add(winer, gbc);
 	}
 	
 	//add action on button
 	private void drawBtnNewGame()
 	{	
-		JButton newGame = new JButton("New game");
+		gbc.gridy = BTN_NEW_GAME_ROW; 
 		
+		JButton newGame = new JButton("New game");
 		newGame.setFont(new Font("Arial", Font.PLAIN, FONT_SIZE)); 
-		newGame.setBounds(BTN_X, FIRST_BTN_Y, BTN_WIDTH, BTN_HEIGHT);
 		
 		//add action
 		
-		this.add(newGame);
+		this.add(newGame, gbc);
 	}
 	
 	private void drawBtnExit()
 	{
-		int y = FIRST_BTN_Y + BTN_HEIGHT + SPACE; 
+		gbc.gridy = BTN_EXIT_ROW; 
 		
 		JButton exit = new JButton("Exit");
-		
 		exit.setFont(new Font("Arial", Font.PLAIN, FONT_SIZE));
-		exit.setBounds(BTN_X, y, BTN_WIDTH, BTN_HEIGHT);
 		
 		exit.addActionListener (new ActionListener () {
 			 public void actionPerformed (ActionEvent e) {
@@ -100,6 +81,6 @@ public class EndingScreen extends Screen
 			 }
 		});
 		
-		this.add(exit);
+		this.add(exit, gbc);
 	}
 }
