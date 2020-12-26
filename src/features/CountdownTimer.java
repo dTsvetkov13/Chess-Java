287ca3b8198f17cb54f7f295aac43365a1cddfe7
admin-45053 <admin-45053@admin-45053-PC>
2020-12-26 23:55:13 +0200

    CountdownTimerTask added and CountdownTimer modified

--- src/features/CountdownTimer.java
+++ src/features/CountdownTimer.java
@@ -5,24 +5,21 @@ import java.util.TimerTask;
 
 public class CountdownTimer 
 {
-	private static int intervalSeconds;
-	private static int intervalMinutes;
 	private static Timer timer;
 	private int seconds;
 	private int minutes;
 	private int delay;
     private int period;
-
+    private CountdownTimerTask timerTask;
 	
-	public CountdownTimer(int seconds, int minutes)
+	public CountdownTimer(int minutes, int seconds)
 	{
 		setSeconds(seconds);
-		intervalSeconds = seconds;
 		setMinutes(minutes);
-		intervalMinutes = minutes;
 		timer = new Timer();
 		delay = 1000;
 	    period = 1000;
+	    timerTask = new CountdownTimerTask(minutes, seconds, timer);
 	}
 
 	public void setSeconds(int seconds)
@@ -47,34 +44,18 @@ public class CountdownTimer
 		return this.minutes;	
 	}
 	
-	public void countdown(int minutes, int seconds)
+	public void countdown()
 	{
-		timer.scheduleAtFixedRate(new TimerTask() 
-		{
-
-	        public void run() 
-	        {
-	        		System.out.println(setIntervalSeconds(getSeconds()));
-	        		if(getSeconds() == 0)
-	        		{
-	        			System.out.println(setIntervalMinutes(getMinutes()));
-	        		}
-
-	        }
-	    }, delay, period);
+		timer.scheduleAtFixedRate(timerTask, delay, period);
+		System.out.println(this.toString());
+		
 	}
 	
-	private static final int setIntervalSeconds(int seconds) 
+	public void stop()
 	{
-		return --intervalSeconds;
+		timer.cancel();
 	}
 	
-	private static final int setIntervalMinutes(int minutes) 
-	{
-		return --intervalMinutes;
-	}
-	
-
 	public String toString()
 	{
 		String result = String.format("Timer: %d : %d", getMinutes(), getSeconds());
--- src/features/TestTimer.java
+++ src/features/TestTimer.java
@@ -4,14 +4,20 @@ import java.util.Scanner;
 
 public class TestTimer {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws InterruptedException 
 	{
 		Scanner sc = new Scanner(System.in);
-		int secs = sc.nextInt();
 		int min = sc.nextInt();
+		int secs = sc.nextInt();
 		CountdownTimer timer = new CountdownTimer(min, secs);
-		timer.countdown(min, secs);
+		timer.countdown();
 		System.out.println(timer.toString());
+		for(int i = 0; i < 3; i++)
+		{
+			Thread.sleep(1000);
+			System.out.println(timer.toString());
+		}
+		timer.stop();
 	}
 
 }
