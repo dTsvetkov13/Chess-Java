dbc8c22309e8056770b2b3c0810f7590b9401f98
Gerganadd <gerganadoncheva2003@gmail.com>
2020-12-26 10:17:50 +0200

    tested GameScreen

--- src/views/screens/TestScreen.java
+++ src/views/screens/TestScreen.java
@@ -7,12 +7,18 @@ public class TestScreen
 	{
 		JFrame window = new JFrame();
 		//MenuScreen menu = new MenuScreen("menu");
-		EndingScreen gameOver = new EndingScreen("Game over");
+		//EndingScreen gameOver = new EndingScreen("Game over");
+		GameScreen game = new GameScreen("chess");
 	
+		window.setLocationRelativeTo(null);
+		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
-		window.setTitle(gameOver.getName());
-		window.add(gameOver);
+		window.setTitle(game.getName());
+		window.add(game);
+		
+		//window.setTitle(gameOver.getName());
+		//window.add(gameOver);
 		
 		//window.setTitle(menu.getName());
 		//window.add(menu);
