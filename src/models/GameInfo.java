6d70c147f625a90595f37398c92b8e1af9fd9a3d
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2020-12-28 11:47:17 +0200

    Singleton implementation added

--- src/models/GameInfo.java
+++ src/models/GameInfo.java
@@ -1,6 +1,23 @@
 package models;
 
+import common.Validator;
+
 public class GameInfo
 {
+	private static GameInfo instance = null;
 	
+	private GameInfo()
+	{
+		
+	}
+	
+	public static GameInfo getInstance()
+	{
+		if(Validator.isNull(instance))
+		{
+			instance = new GameInfo();
+		}
+		
+		return instance;
+	}
 }
