cd0251680d210f3ccdc385c9be1ccf62f8edb276
kids <kids@petia>
2020-12-28 10:07:42 +0200

    Added toString to MovementInDirection (Yova)

--- src/models/MoveTypes.java
+++ src/models/MoveTypes.java
@@ -886,19 +886,6 @@ public class MoveTypes
 		}
 	}
 	
-	public static String toString(Figure f)
-	{
-		Coordinates[] reachable = f.getReachableCells();
-		String result = f.getTeam() + ", " + f.getType();
-		result += "\nCurrent Coordinates: " + f.getCoordinates().getRow() + "; " + f.getCoordinates().getColumn();
-		result += "\nReachable Coordinates: ";
-		for(int i = 0; i < reachable.length; i++)
-		{
-			result += "\n" + reachable[i].getRow() + "; " + reachable[i].getColumn(); 
-		}
-		
-		return result;
-	}
 	
 	
 
