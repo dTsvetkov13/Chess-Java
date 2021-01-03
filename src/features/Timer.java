f83d2243528d9946cb5999badf0a6697b4ec487d
admin-45053 <admin-45053@admin-45053-PC>
2021-01-03 17:37:50 +0200

    Timer in it

---
+++ src/features/Timer.java
@@ -0,0 +1,52 @@
+package features;
+
+public class Timer 
+{
+	private int seconds;
+	private int minutes;
+	private CountdownTimerTask[] timer;
+	
+	public Timer(int minutes, int seconds)
+	{
+		this.minutes = minutes;
+		this.seconds = seconds;
+	}
+	
+	public Timer()
+	{
+		this.minutes = 0;
+		this.seconds = 0;
+	}
+	
+	public int getSeconds()
+	{
+		return this.seconds;
+	}
+	
+	public int getMinutes()
+	{
+		return this.minutes;
+	}
+	
+	public void run() throws InterruptedException
+	{
+		for(int i = 0; i < timer.length; i++)
+		{
+			run();
+			Thread.sleep(1000);
+		}
+	}
+	
+	public void stop()
+	{
+		
+	}
+	
+	public String toString()
+	{
+		String result = String.format("%d : %d", getMinutes(), getSeconds());
+		return result;
+	}
+}
+
+
