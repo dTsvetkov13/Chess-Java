5594d801a7ffd3afb2624c58b5673e42668e4e22
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-01-15 13:15:44 +0200

    figureMovements and movementsUsedBy added

--- src/models/GameInfo.java
+++ src/models/GameInfo.java
@@ -1,9 +1,14 @@
 package models;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Stack;
 
 import common.Constants;
 import common.Validator;
+import enums.FigureType;
+import models.figures.Figure;
 
 public class GameInfo
 {
@@ -11,11 +16,18 @@ public class GameInfo
 	private Player[] players;
 	private int playerOnTurnIndex;
 	private Stack<Move> moves;
+	HashMap<FigureType, ArrayList<Method>> figureMovements;
+	HashMap<Method, ArrayList<FigureType>> movementsUsedBy;
 	
 	private GameInfo()
 	{
 		players = new Player[Constants.PLAYERS_COUNT];
 		moves = new Stack<Move>();
+		figureMovements = new HashMap<FigureType, ArrayList<Method>>();
+		movementsUsedBy = new HashMap<Method, ArrayList<FigureType>>();
+		
+		this.setAllFigureMovements();
+		this.setAllMovementsUsedBy();
 	}
 	
 	public static GameInfo getInstance()
@@ -26,6 +38,72 @@ public class GameInfo
 		}
 		
 		return instance;
+	}
+	
+	private void setAllFigureMovements()
+	{
+		try
+		{
+			figureMovements.put(FigureType.Bishop, new ArrayList<Method>());
+			figureMovements.put(FigureType.Rook, new ArrayList<Method>());
+			figureMovements.put(FigureType.Queen, new ArrayList<Method>());
+			figureMovements.put(FigureType.King, new ArrayList<Method>());
+			figureMovements.put(FigureType.Knight, new ArrayList<Method>());
+			figureMovements.put(FigureType.Pawn, new ArrayList<Method>());
+			
+			this.addMethodToFigureType(FigureType.Bishop, "diagonal");
+			this.addMethodToFigureType(FigureType.Rook, "vertical");
+			this.addMethodToFigureType(FigureType.Queen, "vertical");
+			this.addMethodToFigureType(FigureType.Queen, "diagonal");
+			this.addMethodToFigureType(FigureType.King, "kingMove");
+			this.addMethodToFigureType(FigureType.King, "kingSideCastling");
+			this.addMethodToFigureType(FigureType.King, "queenSideCastling");
+			this.addMethodToFigureType(FigureType.Pawn, "pawnMove");
+			this.addMethodToFigureType(FigureType.Knight, "knightMove");
+		}
+		catch (SecurityException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	private void addMethodToFigureType(FigureType figureType, String methodName)
+	{
+		try
+		{
+			figureMovements.get(figureType).add(MoveTypes.class.getMethod(methodName, Figure.class));
+		}
+		catch (NoSuchMethodException e)
+		{
+			e.printStackTrace();
+		}
+		catch (SecurityException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	private void setAllMovementsUsedBy()
+	{
+		try
+		{
+			for(var figureType : this.figureMovements.keySet())
+			{
+				for(var method : this.figureMovements.get(figureType))
+				{
+					if(!this.movementsUsedBy.containsKey(method))
+					{
+						this.movementsUsedBy.put(method, new ArrayList<FigureType>());
+					}
+					
+					this.movementsUsedBy.get(method).add(figureType);
+				}
+			}	
+		}
+		catch (SecurityException e)
+		{
+			e.printStackTrace();
+		}
 	}
 	
 	public void changeTurn()
@@ -79,4 +157,6 @@ public class GameInfo
 	{
 		return this.moves.get(this.moves.size() - 1);
 	}
+	
+	//Add undo method
 }
